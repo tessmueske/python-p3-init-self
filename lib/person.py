7ab5f5864286f283f5fcbc8a0dba7ead69ddd8d2
Tess <tess.mueske@gmail.com>
2024-06-07 13:43:39 +0200

    complete lab

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+
+# Person.__init__ with a Name
+# Define a Person class in lib/person.py. In the class, define an __init__ method that accepts an argument for the person's name. That argument should be stored within a self.name attribute.
